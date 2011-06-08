9337cb84a88a4c004230e28901e891d877ac83c4
Daniel Mrózek <mrazicz@gmail.com>
2011-06-08 17:55:15 +0200

    Backtime v1.1

--- init.rb
+++ init.rb
@@ -14,8 +14,6 @@ Redmine::Plugin.register :redmine_backtime do
   url 'https://github.cz/mrazicz/redmine_backtime'
   author_url 'https://github.cz/mrazicz'
   
-  ###permission :backtime_plugin, { :backtime => [:new, :add] }, :require => :member_of_backtime
-
   menu :top_menu, :backtime, { :controller => 'backtime', :action => 'index' }, :caption => 'BackTime', :before => :help,
        # shows BackTime tab only if current user is member of BackTime group or admin
        :if => Proc.new { User.current.group_ids.include?(Group.find_by_lastname("BackTime").id) || User.current.admin? }
