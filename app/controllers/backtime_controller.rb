9e7510d39080fded8085940a5b3d120aac09a897
Daniel Mrózek <mrazicz@gmail.com>
2011-07-30 15:03:56 +0200

    Bugfix in backtime_controller

--- app/controllers/backtime_controller.rb
+++ app/controllers/backtime_controller.rb
@@ -1,55 +1,55 @@
 class BacktimeController < ApplicationController
   unloadable
 
-  before_filter :member_of_backtime
+  before_filter :member_of_backtime, :check_db_backtime
 
-  def index    
+  def index
     @users_list = Group.find_by_lastname('Backtime').users.all(:select => "users.*, SUM(backtimes.time) as times_total",
                            :joins => "LEFT JOIN backtimes AS backtimes ON backtimes.partner_id = users.id",
                            :group => "users.id",
-                           :conditions => ["users.id != ?", User.current.id], 
+                           :conditions => ["users.id != ?", User.current.id],
                            :order => "times_total DESC"
                           )
-    
+
     unless @users_list.nil?
       @users_list.map! {
-        |u| [u.lastname + ' ' + u.firstname, u.id] 
+        |u| [u.lastname + ' ' + u.firstname, u.id]
       }
     end
-    
+
     # id of current user
     cuser = User.current.id
     # tmp var for sum time and backtime
-    tmp_sum1 = Backtime.find(:all, :conditions => ["user_id = ?", cuser]).to_a    
-    tmp_sum2 = Backtime.find(:all, :conditions => ["partner_id = ?", cuser]).to_a 
+    tmp_sum1 = Backtime.find(:all, :conditions => ["user_id = ?", cuser]).to_a
+    tmp_sum2 = Backtime.find(:all, :conditions => ["partner_id = ?", cuser]).to_a
 
     @time_sum = tmp_sum1.sum(&:time) + tmp_sum2.sum(&:back_time)
     @backtime_sum = tmp_sum1.sum(&:back_time) + tmp_sum2.sum(&:time)
 
-    backtimes_with_issue = Backtime.all(:select => "backtimes.*, SUM(backtimes.time) as times", 
-                                         :conditions => ["time_entry_id IS NOT NULL AND (backtimes.user_id = ? OR backtimes.partner_id = ?)", cuser, cuser], 
-                                         :joins => "LEFT JOIN time_entries AS te ON te.id = time_entry_id", 
+    backtimes_with_issue = Backtime.all(:select => "backtimes.*, SUM(backtimes.time) as times",
+                                         :conditions => ["time_entry_id IS NOT NULL AND (backtimes.user_id = ? OR backtimes.partner_id = ?)", cuser, cuser],
+                                         :joins => "LEFT JOIN time_entries AS te ON te.id = time_entry_id",
                                          :group => 'te.issue_id')
     backtimes_without_issue = Backtime.all(:conditions => ["time_entry_id IS NULL AND (user_id = ? OR partner_id = ?)", cuser, cuser])
     backtimes_all = backtimes_with_issue + backtimes_without_issue
 
     backtimes_all.each do |b|
       unless b['times'].nil?
-        b['time'] = b['times']      
+        b['time'] = b['times']
         b['description'] = "<a href='/issues/#{b.time_entry.issue_id}'> ##{b.time_entry.issue_id}</a>: "
         b['description'] += b.time_entry.issue.subject
         b['description'] += " [<a href='/projects/#{b.time_entry.issue.project.identifier}'>#{b.time_entry.issue.project.name}</a>]"
       end
     end
     backtimes_all = backtimes_all.sort_by(&:created_at).reverse
-    
+
     # paginate results
     @backtimes_count = backtimes_all.size
     @backtimes_pages = Paginator.new self, @backtimes_count, per_page_option, params['page']
     limit = @backtimes_pages.items_per_page
     offset = @backtimes_pages.current.offset
     @backtimes = backtimes_all[offset..offset+limit]
-    
+
     @backtime = Backtime.new
   end
 
@@ -72,4 +72,16 @@ class BacktimeController < ApplicationController
       render_403
     end
   end
+
+  def check_db_backtime
+    null_times = Backtime.find_all_by_time(nil)
+    null_times.each do |nt|
+      nt.update_attribute(:time, 0)
+    end
+    null_backtimes = Backtime.find_all_by_back_time(nil)
+    null_backtimes.each do |nbt|
+      nbt.update_attribute(:back_time, 0)
+    end
+  end
 end
+
--- app/models/backtime.rb
+++ app/models/backtime.rb
@@ -1,11 +1,12 @@
 class Backtime < ActiveRecord::Base
   unloadable
-  
+
   belongs_to :partner, :class_name => 'User', :foreign_key => :partner_id
   belongs_to :user, :class_name => 'User', :foreign_key => :user_id
   belongs_to :time_entry
 
   validates_presence_of :partner_id
-  #validates_numericality_of :time, :back_time
+  validates_numericality_of :time, :back_time
 
 end
+
